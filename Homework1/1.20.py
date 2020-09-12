897fce7dfd79bb980ff9cbb928b320489624dd2a
CalebCov <70293129+CalebCov@users.noreply.github.com>
2020-09-12 14:11:26 -0500

    Add files via upload
    1.20 CIS 2348 LAB: Warm Up: Basic output with variables.

---
+++ Homework1/1.20.py
@@ -0,0 +1,16 @@
+user_num = int(input("Enter integer:\n"))
+
+# Type your code here
+num_squared = user_num * user_num
+num_cubed = user_num * user_num * user_num
+
+print("You entered:",user_num)
+print(user_num, "squared is",num_squared)
+print("And",user_num,"cubed is",num_cubed,"!!")
+
+user_num2 = int(input("Enter another integer:\n"))
+sum = user_num + user_num2
+product = user_num * user_num2
+
+print(user_num,"+",user_num2,"is",sum)
+print(user_num,"*",user_num2,"is",product)

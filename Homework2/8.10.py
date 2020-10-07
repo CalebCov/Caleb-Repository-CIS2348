1e621017d3ad91d25cdca5a69bc257634721bbba
CalebCov <70293129+CalebCov@users.noreply.github.com>
2020-10-07 11:51:46 -0500

    Add files via upload

---
+++ Homework2/8.10.py
@@ -0,0 +1,11 @@
+user_input = input()
+normal = ""
+reverse = ""
+for character in user_input.lower():
+    if character != ' ':
+        normal += character
+        reverse = character + reverse
+if normal == reverse:
+    print(user_input + " is a palindrome")
+else:
+    print(user_input + " is not a palindrome")

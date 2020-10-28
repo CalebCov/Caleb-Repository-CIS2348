340295922c038ed6f88249041295fb74cc4e19c9
CalebCov <70293129+CalebCov@users.noreply.github.com>
2020-10-28 02:30:57 -0500

    Add files via upload

---
+++ Homework 3/11.18.py
@@ -0,0 +1,9 @@
+integers = input().split()
+non_negative=[]
+for num in integers:
+    num=int(num)
+    if num>=0:
+        non_negative.append(num)
+non_negative.sort()
+for i in non_negative:
+    print(i,end=" ")
---
+++ Homework 3/11.22.py
@@ -0,0 +1,8 @@
+wordlist= list(input().split(" "))
+wordfreq = []
+
+for l in wordlist:
+    wordfreq.append(wordlist.count(l))
+
+for i in range(len(wordlist)):
+    print(wordlist[i], wordfreq[i])
